f19ed37f84e8ae39523281f913ba7c8349aad481
Shafin580 <shafinwork580@gmail.com>
2022-03-28 14:47:03 +0600

    Creating Projectiles

--- index.js
+++ index.js
@@ -43,9 +43,37 @@ class Projectile {
   }
 }
 
+class Astroid {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+// Creating Player
+
 const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
 
 const projectiles = [];
+const astroids = [];
+
+// Shooting Projectiles
 
 window.addEventListener("click", (event) => {
   const angle = Math.atan2(
@@ -61,6 +89,34 @@ window.addEventListener("click", (event) => {
   );
 });
 
+// Creating Astroids
+
+function spawnAstroid() {
+  setInterval(() => {
+    const radius = Math.random() * (30-5) + 5;
+    let x;
+    let y;
+    if (Math.random < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+      //const y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const color = "green";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    astroids.push(new Astroid(x, y, radius, color, velocity));
+  }, 1000);
+}
+
+// Looping animation
+
 function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -68,6 +124,11 @@ function animate() {
   projectiles.forEach((projectile) => {
     projectile.update();
   });
+
+  astroids.forEach((astroid) => {
+    astroid.update();
+  });
 }
 
 animate();
+//spawnAstroid();
